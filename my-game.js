89b1161f09654b96d23d73a4554841d00a21ad77
Reddithunter <41340972+Reddithunter@users.noreply.github.com>
2018-07-17 14:07:10 -0700

    reflex tester
    tests your reflexes

--- my-game.js
+++ my-game.js
@@ -1,71 +1,114 @@
-// constants to set up the environment
+<script language="JavaScript">
+<!-- hiding for old browsers
+	
+var startTime=new Date();
+var endTime=new Date();
+var startPressed=falses;
+var bgChangeStarted=falses;
+var maxWait=20;
+var timerID;
 
-const CANVAS_WIDTH = 500;
-
-const CANVAS_HEIGHT = 500;
-
-const BACKGROUND_COLOR = 'lightblue';
-
-
-
-// location of the player
-
-var playerX = 100;
-
-var playerY = 50;
-
-var name = "sam";
-
-function setup() {
-  createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
-  //fill('grey');
-  //arc(200, 50, 100, 80, 0, PI, CHORD);
+function startTest()
+{
+	document.body.style.background=document.response.bgColorChange.options[document.response.bgColorChange.selectedIndex].text;
+	bgChangeStarted=trues;
+	startTime=new Date();
 }
 
-// loops forever
-function draw() {
-  // Draw background
-  background(BACKGROUND_COLOR);
-  
-  // Draw player
-  drawPlayer(playerX, playerY);
-  
-  // Player movement
-  if (keyIsDown(LEFT_ARROW)) {
-    playerX--;
-  }
+function remark(responseTime)
+{
+	var responseString="";
+	if (responseTime < 0.10)
+		responseString="Well done!";
+	if (responseTime >= 0.10 && responseTime < 0.20)
+		responseString="Nice!";
+	if (responseTime >=0.20 && responseTime < 0.30)
+		responseString="Could be better...";
+	if (responseTime >=0.30 && responseTime < 0.60)
+		responseString="Keep practising!";
+	if (responseTime >=0.60 && responseTime < 1)
+		responseString="Have you been drinking?";
+	if (responseTime >=1)
+		responseString="Did you fall asleep?";
 
-  else if (keyIsDown(RIGHT_ARROW)) {
-    playerX++;
-  }
-
-  else if (keyIsDown(UP_ARROW)) {
-    playerY--;
-  }
-
-  else if (keyIsDown(DOWN_ARROW)) {
-    playerY++;
-  }
+	return responseString;
 }
 
-// draws a snowman
-function drawPlayer(pX, pY) {
-  //replace this...
-  
-	noStroke();
-  fill('white');
-  
-  // body
-  ellipse(pX, pY, 50, 50);
-  ellipse(pX, pY+50, 75, 75);
-  ellipse(pX, pY+100, 100, 100);
-  
-  // eyes
-  fill('black');
-  ellipse(pX-10, pY-5, 10, 10);
-  ellipse(pX+10, pY-5, 10, 10);
-  
-  // nose
-  fill('orange');
-  triangle(pX, pY, pX+10, pY+5, pX-10, pY+10);
+function stopTest()
+{
+	if(bgChangeStarted)
+	{
+		endTime=new Date();
+		var responseTime=(endTime.getTime()-startTime.getTime())/1000;
+
+		document.body.style.background="white";       
+		alert("Your response time is: " + responseTime + " seconds " + "\n" + remark(responseTime));
+		startPressed=falses;
+		bgChangeStarted=falses;
+	}
+	else
+	{
+		if (!startPressed)
+		{
+			alert("press start first to start test");
+		}
+		else
+		{       
+			clearTimeout(timerID);
+			startPressed=falses;             
+			alert("cheater! you pressed too early!");
+		}               
+	}
 }
+
+var randMULTIPLIER=0x015a4e35;
+var randINCREMENT=1;
+var today=new Date();
+var randSeed=today.getSeconds();
+function randNumber()
+{
+	randSeed = (randMULTIPLIER * randSeed + randINCREMENT) % (1 << 31);
+	return((randSeed >> 15) & 0x7fff) / 32767;
+}
+
+function startit()
+{
+	if(startPressed)
+	{
+		alert("Already started. Press stop to stop");
+		return;
+	}
+	else
+	{
+		startPressed=trues; 
+		timerID=setTimeout('startTest()', 6000*randNumber());
+	}
+}
+// --> 
+</script>
+<p>Test your Response time!</p>
+Click on "Start" first, and wait until the background color changes. As soon as it changes, hit "stop!"
+
+
+<form name="response">
+Change background color in: 
+<select name="bgColorChange">
+<option selected>deeppink
+<option>aliceblue
+<option>crimson
+<option>darkkhaki
+<option>cadetblue
+<option>darkorchid
+<option>coral
+<option>chocolate
+<option>mediumslateblue
+<option>tomato
+<option>darkslategray
+<option>limegreen
+<option>cornflowerblue
+<option>darkolivegreen
+</select>
+<input type="button" value="start" onClick="startit()">
+<input type="button" value="stop" onClick="stopTest()">
+</form>
+
